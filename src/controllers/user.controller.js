a59d8892c06c8131b1ab6eeaf8f082bda003366e
Shrey Kariria <shrey.kariria1910@gmail.com>
2025-03-15 20:26:45 +0530

    add register controller and bug fixes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,71 @@
 import {asyncHandler} from '../utils/asyncHandler.js'
+import {apiError} from '../utils/apiError.js'
+import {User} from '../models/user.model.js'
+import { uploadOnCloudinary } from '../utils/cloudnary_file_uploader.js'
+import { apiResponse } from '../utils/apiResponse.js'
 
 const registerUser = asyncHandler(async (req,res)=>{
-    res.status(200).json({
-        message: "ok"
+
+    // get user details from frontend
+    // validation - not empty
+    // check if user already exists: username, email
+    // check for images, check for avatar
+    // upload them to cloudinary, avatar
+    // create user object - create entry in db
+    // remove password and refresh token field from response
+    // check for user creation
+    // return res
+
+    const {username, email, password, fullName} = req.body
+    console.log("email", email)
+    // from and json data body se milta h url wala body se nahi milta hai
+
+    if(
+        [fullName, email, username, password].some((field)=> field?.trim()==="")
+    ){
+        throw new apiError(400, "all fields are required")
+    }
+
+    const exitedUser = User.findOne({
+        $or: [{username},{email}]
     })
+
+    if(exitedUser){
+        throw new apiError(409,"User already exit!")
+    }
+
+    const avatarLocalPath = req.files?.avator[0]?.path;
+    const coverIamgeLocalpath = re.files?.coverImage[0]?.path;
+
+    if(!avatarLocalPath){
+        throw new apiError(400, "avatar file is required")
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    const coverIamge = await uploadOnCloudinary(coverIamgeLocalpath)
+    
+    if(!avatar){
+        throw new apiError(400, "avatar file is required")
+    }
+
+    const user = await User.create({
+        fullName,
+        avatar: avatar.url,
+        coverIamge: coverIamge?.url || "",
+        email,
+        password,
+        username: username.toLowerCase(),
+    })
+
+    const createdUser = await User.findById(user._id).select("-password -refreshToken")
+    if(!createdUser){
+        throw new apiError(500, "Something went wrong while registering the User")
+    }
+
+    return res.status(201).json(
+        new apiResponse(200, createdUser, "User registered successfully")
+    )
+
 })
 
 export {registerUser}
