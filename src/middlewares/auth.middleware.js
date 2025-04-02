289850226707cdc81310e76a40a1fa798ba7c870
Shrey Kariria <shrey.kariria1910@gmail.com>
2025-04-02 14:45:08 +0530

    bug fixes

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -5,7 +5,12 @@ import { User } from "../models/user.model.js";
 
 export const verifyJWT = asyncHandler(async(req, _, next)=>{
     try {
-        const token = req.cookie?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+
+        // console.log("Cookies:", req.cookies);
+        // console.log("Auth Header:", req.header("Authorization"));
+
+        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+        // console.log(token)
     
         if(!token){
             throw new apiError(401, "Unauthorized Request")
