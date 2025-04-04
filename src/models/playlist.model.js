beb6c96fbd2f2d0d3da4248e749788ac79009acd
Shrey Kariria <shrey.kariria1910@gmail.com>
2025-04-05 00:48:31 +0530

    add comment, like, tweet and playlist models

---
+++ src/models/playlist.model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const playlistSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            require: true,
+        },
+        description: {
+            type: String,
+            require: true,
+        },
+        vidoes: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Video"
+            }
+        ],
+        owner: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+export const Playlist = mongoose.model("Playlist", playlistSchema)
