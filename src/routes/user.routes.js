4f4cf8669d5e800ce3627f241093fcbfde9cf42f
Shrey Kariria <shrey.kariria1910@gmail.com>
2025-04-04 06:30:58 +0530

    add new routes for user controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import {Router} from 'express'
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from '../controllers/user.controller.js'
+import { chnageCurrentPassword, getCurrentUser, getUserChannelProfile, getWatchHistory, loginUser, logoutUser, refreshAccessToken, registerUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage } from '../controllers/user.controller.js'
 import {upload} from '../middlewares/multer.middleware.js'
 import { verifyJWT } from '../middlewares/auth.middleware.js'
 
@@ -23,7 +23,16 @@ router.route("/login").post(loginUser)
 //secured routes
 
 router.route("/logout").post(verifyJWT, logoutUser)
-
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT, chnageCurrentPassword)
+router.route("/current-user").post(verifyJWT, getCurrentUser)
+router.route("/update-ccount").patch(verifyJWT, updateAccountDetails)
+router.route("/avatar-update").patch(verifyJWT, upload.single("avatar"),  updateUserAvatar)
+router.route("/cover-image-update").patch(verifyJWT, upload.single("coverImage"),  updateUserCoverImage)
+router.route("/watch-history").get(verifyJWT, getWatchHistory)
+
+// taking input from params
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+
 
 export default router
